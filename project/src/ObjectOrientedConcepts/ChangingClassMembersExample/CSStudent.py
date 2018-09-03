e7d572195b161c8592e618b25161456fbab2261b
Timothy John Manas <timothy.john.manas@gmail.com>
2018-09-02 22:09:57 -0400

    Modifying the Class member static values which affect ALL objects which are instance of that object

---
+++ project/src/ObjectOrientedConcepts/ChangingClassMembersExample/CSStudent.py
@@ -0,0 +1,11 @@
+class CSStudent:
+
+    stream = "Computer Science"
+
+    def __init__(self, name, id):
+        self.name = name
+        self.id = id
+
+
+
+    pass
---
+++ project/src/ObjectOrientedConcepts/ChangingClassMembersExample/ChangingClassMembersExample.py
@@ -0,0 +1,50 @@
+from project.src.ObjectOrientedConcepts.ChangingClassMembersExample.CSStudent import *
+
+def main():
+
+    student1 = CSStudent("Tim", 12345)
+    student2 = CSStudent("John", 346)
+    student3 = CSStudent("Romero", 233424234234234)
+
+    #Print Student Stream
+    print("Stream of Student1: ", student1.stream)
+    print("Stream of Student2: ", student2.stream)
+    print("Stream of Student3: ", student3.stream)
+
+    # Changing the member of the Class by referring to the CLASS NAME directly
+    CSStudent.stream = "Mathematics"
+
+    #Print Student Stream
+    print("\nStream of Student1: ", student1.stream)
+    print("Stream of Student2: ", student2.stream)
+    print("Stream of Student3: ", student3.stream)
+
+    # Now we only want Student#3 to be back to BIOLOGY
+    student3.stream = "BIOLOGY"
+
+    #Print Student Stream
+    print("\nStream of Student1: ", student1.stream)
+    print("Stream of Student2: ", student2.stream)
+    print("Stream of Student3: ", student3.stream)
+
+
+
+    pass
+
+
+if __name__ == '__main__':
+    main()
+
+
+'''
+Theres going to be instances when we want to change the static members for ALLLLLLL the classes 
+
+If we want to change the instance of all the object which REFER to that class
+- We must use the CLASS NAME instead of the object name
+
+But if we want to change individual objects
+- Then we must use the individual object name member
+
+
+
+'''
