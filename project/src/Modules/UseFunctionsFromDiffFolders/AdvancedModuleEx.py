d78959a4805dd1e128beb0b6dd2ebeecb0a7d6b2
Timothy John Manas <timothy.john.manas@gmail.com>
2019-07-02 21:20:52 -0400

    Added additional Module Recipe

---
+++ project/src/Modules/UseFunctionsFromDiffFolders/AdvancedModuleEx.py
@@ -0,0 +1,31 @@
+from project.src.Modules.ModuleExample.ModuleXYZ import *
+
+def main():
+
+    # Using add(x,y) from the ModuleExample
+    # Notice this is in completely different module
+    print("Add: ", add(5,4))
+
+
+
+
+if __name__ == '__main__':
+    main()
+
+
+'''
+Note 
+
+- By Default, you CANNOT import files from a different python "FOLDER" (FOLDER ...NOT PACKAGE)
+
+Packages
+- Carry the __init__.py. This REQUIRED  to be used as part of a package
+- If you remove this file - Python will no longer look for sub modules in that directory
+    -> ANY IMPORTS from this file will fail
+    
+    
+Note
+- The __init__.py file is needed for Python 2.X but no longer required for 3.3 and Above    
+    
+
+'''
